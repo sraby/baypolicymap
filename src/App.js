9e3df49486516a9bee7f07592303b48adbad15ac
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-06-07 17:16:30 -0400

    Added documentation and removed unneeded code

--- src/App.js
+++ src/App.js
@@ -14,17 +14,40 @@ constructor() {
     tileURL: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   }
+
 }
+
+// // JSON Properties: 
+// "cartodb_id": number,
+// "condoconv": 0 | 1,
+// "city": string,
+// "justcause": 0 | 1,
+// "stabilizat": 0 | 1,
+// "reviewboar": 0 | 1,
+// "mobilehome": 0 | 1,
+// "sropres": 0 | 1,
+// "foreclosur": 0 | 1,
+// "jobshousin": 0 | 1,
+// "commercial": 0 | 1,
+// "trustfund": 0 | 1,
+// "inclusiona": 0 | 1,
+// "densitybon": 0 | 1,
+// "landtrust": 0 | 1,
+// "firstsourc": 0 | 1,
+// "total": number
+
 
 render() {
 
-  const position = [this.state.lat, this.state.lng]
+  const position = [this.state.lat, this.state.lng];
+
     return (
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer url={this.state.tileURL} attribution={this.state.attribution} subdomains={'abcd'} />
         <GeoJSON data={data} />
       </Map>
     );
+
   }
 }
 
