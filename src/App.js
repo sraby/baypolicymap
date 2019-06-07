46db4550ec56b6d452ea5ae31c2c9849e903768f
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-06-07 16:57:49 -0400

    initial commit from create react app
    added Positron Basemap and geojson data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, TileLayer, GeoJSON} from 'react-leaflet';
+import data from './data.json';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+constructor() {
+  super()
+  this.state = {
+    lat: 37.7,
+    lng: -123.1,
+    zoom: 9,
+    tileURL: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  }
+}
+
+render() {
+
+  const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer url={this.state.tileURL} attribution={this.state.attribution} subdomains={'abcd'} />
+        <GeoJSON data={data} />
+      </Map>
+    );
+  }
 }
 
 export default App;
