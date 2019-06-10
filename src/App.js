6a6aeb2880130e29789d91f867f0d2bb5a8b022e
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-06-09 22:20:59 -0400

    added styling and city/policy selectors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Map, Pane, TileLayer, GeoJSON, ZoomControl} from 'react-leaflet';
-import data from './data.json';
-//import './App.css';
+import mapData from './data/mapData.json';
+
+import './App.scss'
 
 const getColorTotal = function(d) {
   return d < 1 ? '#e1dbca' :
@@ -38,8 +39,34 @@ constructor() {
     },
   }
 
-  this.policyCodeNames = Object.keys(data.features[0].properties).filter(x => !["cartodb_id","city","total"].includes(x));
+  this.policyList = [
+     {code: "condoconv", name:"Condominium conversion regulations"},
+     {code: "justcause", name:"Just Cause Eviction Ordinance"},
+     {code: "stabilizat", name:"Rent Stabilization or Rent Control"},
+     {code: "reviewboar", name:"Rent Review Board and/or Mediation"},
+     {code: "mobilehome", name:"Mobile home rent control"},
+     {code: "sropres", name:"SRO preservation"},
+     {code: "foreclosur", name:"Foreclosure assistance"},
+     {code: "jobshousin", name:"Jobs-housing linkage fee"},
+     {code: "commercial", name:"Commerical linkage fee"},
+     {code: "trustfund", name:"Housing trust fund"},
+     {code: "inclusiona", name:"Inclusionary zoning"},
+     {code: "densitybon", name:"Density bonus ordinance"},
+     {code: "landtrust", name:"Community Land Trusts"},
+     {code: "firstsourc", name:"First source hiring"}
+  ];
 
+  this.cityList = mapData.features.reduce((list, feature) => {
+      list.push(feature.properties.city);
+      return list; 
+    }, []).sort();
+}
+
+resetPosition = () => {
+  this.setState({
+    lat: 37.7,
+    lng: -122.6
+  })
 }
 
 updateStyle = (focusCity, focusPolicy) => {
@@ -97,10 +124,43 @@ render() {
             url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png" />
         </Pane>
         <GeoJSON 
-          data={data} 
+          data={mapData} 
           style={this.state.style} 
           onEachFeature={this.onEachFeature} />
         <ZoomControl position='topright' />
+
+        <div className="dropdown dropdown-right policy-selector">
+          <a href="#" className="btn bg-light dropdown-toggle" tabIndex="0">
+            Map a specific policy... <i className="icon icon-caret"></i>
+          </a>
+          <ul className="menu">
+            <li className="menu-item" key="total">
+              <a onClick={() => this.updateStyle(this.state.focusCity, "total")}>Count of anti-displacement policies</a>
+            </li>
+            <li className="divider"></li>
+            {this.policyList.map( (policy) => 
+              <li className="menu-item" key={policy.code}>
+                <a onClick={() => this.updateStyle(this.state.focusCity, policy.code)}>{policy.name}</a>
+              </li> )}
+          </ul>
+        </div>
+
+        <div className="dropdown dropdown-right city-selector">
+          <a href="#" className="btn bg-light dropdown-toggle" tabIndex="0">
+            Find a city... <i className="icon icon-caret"></i>
+          </a>
+          <ul className="menu">
+            <li className="menu-item" key="total">
+              <a onClick={() => this.resetPosition()}>Show All</a>
+            </li>
+            <li className="divider"></li>
+            {this.cityList.map( (cityName) => 
+              <li className="menu-item" key={cityName}>
+                <a onClick={() => this.updateStyle(cityName, this.state.focusPolicy)}>{cityName}</a>
+              </li> )}
+          </ul>
+        </div>
+
       </Map>
     );
 
