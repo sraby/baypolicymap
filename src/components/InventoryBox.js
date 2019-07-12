28052f0bb29c203be8de6340a01dd1ff4a8c1132
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-07-11 22:45:49 -0400

    added scroll-to-top updating to inventory box

--- src/components/InventoryBox.js
+++ src/components/InventoryBox.js
@@ -1,36 +1,43 @@
 import React from 'react';
 
-const InventoryBox = (props) => {
-    const sourceList = props.cityData && [1,2,3,4,5,6,7,8]
-        .map(i => props.cityData[("source" + i)])
-        .filter(e => e);
+class InventoryBox extends React.Component { 
 
-    return (
-        <div className="inventory-box uk-padding-small">
-            <h5 className="uk-text-bold">
-                {props.cityData ?
-                props.cityData.city + ': ' + props.cityData.total + ' policy measure' + (props.cityData.total === 1 ? '':'s') : 
-                'Choose a city from the map to see its anti-displacement policy measures'}
-            </h5>
-            <ul className="inventory-list uk-list uk-list-divider">
-                {props.policyList.map(policy =>
-                    <li key={policy.code} className="inventory-list-item">
-                        <span className={(props.cityData && props.cityData[policy.code].slice(0,2).toUpperCase() !== 'NO') ? 'uk-text-bold' : ''}>
-                            {policy.name} <a className="uk-text-secondary" href={('#' + policy.code)} >ⓘ</a>: {(props.cityData && props.cityData[policy.code])}
+    componentDidUpdate() {
+        document.querySelector('.inventory-box').scrollTop = 0;
+    }
+
+    render() {
+        const sourceList = this.props.cityData && [1,2,3,4,5,6,7,8]
+            .map(i => this.props.cityData[("source" + i)])
+            .filter(e => e);
+
+        return (
+            <div className="inventory-box uk-padding-small">
+                <h5 className="uk-text-bold">
+                    {this.props.cityData ?
+                    this.props.cityData.city + ': ' + this.props.cityData.total + ' policy measure' + (this.props.cityData.total === 1 ? '':'s') : 
+                    'Choose a city from the map to see its anti-displacement policy measures'}
+                </h5>
+                <ul className="inventory-list uk-list uk-list-divider">
+                    {this.props.policyList.map(policy =>
+                        <li key={policy.code} className="inventory-list-item">
+                            <span className={(this.props.cityData && this.props.cityData[policy.code].slice(0,2).toUpperCase() !== 'NO') ? 'uk-text-bold' : ''}>
+                                {policy.name} <a className="uk-text-secondary" href={('#' + policy.code)} >ⓘ</a>: {(this.props.cityData && this.props.cityData[policy.code])}
+                            </span>
+                        </li>
+                    )}
+                </ul>
+                <span className="inventory-sources"> 
+                    {sourceList && sourceList.map((url,i) =>
+                        <span className="inventory-source-link" key={i + 1}>
+                            {(i > 0) && ', '}
+                            <a href={url} target="_blank" rel="noopener noreferrer">Source {i + 1}</a>
                         </span>
-                    </li>
-                )}
-            </ul>
-            <span className="inventory-sources"> 
-                {sourceList && sourceList.map((url,i) =>
-                    <span className="inventory-source-link" key={i + 1}>
-                        {(i > 0) && ', '}
-                        <a href={url} target="_blank" rel="noopener noreferrer">Source {i + 1}</a>
-                    </span>
-                )}
-            </span>
-        </div>
-    );
+                    )}
+                </span>
+            </div>
+        );
+    }
 }
 
 export default InventoryBox; 
